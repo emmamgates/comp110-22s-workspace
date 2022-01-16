da952ee2f5f03e17d0e20c19abd192cd3efd07b0
Emmamgates <memma@ad.unc.edu>
2022-01-15 23:29:29 -0500

    finished exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,6 @@
+"""my first program for COMP110"""
+
+__author__ = "730399828"
+
+
+print("Hello, world!!")
---
+++ exercises/ex00_hello_world_1.py
@@ -0,0 +1,4 @@
+print("Hello, World!!")
+"""my first program for comp110"""
+
+__author__ = "730399828"
